3911ec9038b041f695325d1a1ce0fba815e4e436
Esemono <Esemono@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-02-02 18:17:22 +0000

    update to 0.1.8 (jQuery noConflict fix)
    git-svn-id: https://plugins.svn.wordpress.org/instagram-for-wordpress/tags/0.1.8@340059 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/wpinstagram.js
@@ -0,0 +1,12 @@
+jQuery.noConflict()(function(){
+	jQuery("div.wpinstagram").cycle({fx: "fade"});
+	jQuery("div.wpinstagram").find("a").fancybox({
+		"transitionIn":			"elastic",
+		"transitionOut":		"elastic",
+		"easingIn":				"easeOutBack",
+		"easingOut":			"easeInBack",
+		"titlePosition":		"over",
+		"padding":				0,
+		"hideOnContentClick":	"true"
+	});
+});
