161a7c8b36c1375e981e506cd3d021272b86b62b
jbenders <jbenders@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-01 11:55:56 +0000

    quickfix for user search autocomplete
    git-svn-id: https://plugins.svn.wordpress.org/instagram-for-wordpress/tags/2.0.4@1171836 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- templates/setupButton.php
+++ templates/setupButton.php
@@ -471,7 +471,7 @@
       //our dropdown search
       jQuery('#otherUser').keyup(function(event) {
         setTimeout(function() {
-          searchUserHandler();
+          searchUserHandler<?php print $instance['db_id'] ?>();
         }, 200);
       });
       jQuery('#otherUser').blur(function(event) {
@@ -671,8 +671,8 @@
         }, 100);
       }
     
-      if (!window.searchUserHandler) {
-        window.searchUserHandler = function() {
+      if (!window.searchUserHandler<?php print $instance['db_id'] ?>) {
+        window.searchUserHandler<?php print $instance['db_id'] ?> = function() {
           var keywords = jQuery('#otherUser').val();
                  
           if (keywords.length > 2) {
